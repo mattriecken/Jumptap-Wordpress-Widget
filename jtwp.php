6d9c0e2e908e0faa09d8b749898954296a8c3d93
tomkel5 <tom@tomkel.com>
2012-03-15 14:44:48 -0400

    Adding blank admin page

--- jtwp.php
+++ jtwp.php
@@ -11,8 +11,6 @@ Version: 0.1
 
 function turn_body_blue($classes)
 {
-	//echo $_SERVER['HTTP_USER_AGENT'];
-	
 	if (preg_match("/chrome/i", $_SERVER['HTTP_USER_AGENT']))
 		$classes[] = "blue";
 	
@@ -20,5 +18,33 @@ function turn_body_blue($classes)
 }
 
 add_filter( "body_class", "turn_body_blue" );
+
+
+
+
+
+
+
+
+
+
+
+function jtwp_plugin_menu() {
+	add_options_page('JTWP Options', 'JTWP', 'manage_options', 'jtwp_plugin', 'jtwp_plugin_options');
+}
+
+function jtwp_plugin_options() {
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
+
+add_action('admin_menu', 'jtwp_plugin_menu');
+
+
+
 
 ?>
